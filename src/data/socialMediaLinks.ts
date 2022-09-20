2842b850a00d8036cf549bab8ef71233d201dedd
TrakaDev <trakadev@gmail.com>
2022-09-20 16:24:16 -0500

    [ADD] Social Media Links

---
+++ src/data/socialMediaLinks.ts
@@ -0,0 +1,6 @@
+export const links = {
+  twitter: "https://twitter.com/BeardedBuddies_",
+  tiktok: "https://www.tiktok.com/@beardedbudd13es",
+  discord: "https://discord.gg/aWjmfuucZa",
+  instagram: "https://www.instagram.com/officialbeardedbuddies",
+}
