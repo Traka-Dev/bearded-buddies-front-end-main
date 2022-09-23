37e4c533fbfbbaf9dee8841e5c761e34c5792380
TrakaDev <trakadev@gmail.com>
2022-09-23 12:01:35 -0500

    [FIX] Collection & Team & Subscribe

--- src/components/NftSection/index.tsx
+++ src/components/NftSection/index.tsx
@@ -1,163 +1,179 @@
-import React, { useEffect, useState } from "react";
-import backgroundImage from "../../assets/images/nft-bg.png";
-import logo from "../../assets/images/logo.png";
-import Carousel from "./Carousel";
-import { strings } from "../../data/nftSection";
-import { isMobile } from "react-device-detect";
-import styles from "./index.module.scss";
+import React, { useEffect, useState } from "react"
+import backgroundImage from "../../assets/images/nft-bg.png"
+import logo from "../../assets/images/logo.png"
+import Carousel from "./Carousel"
+import { strings } from "../../data/nftSection"
+import { isMobile } from "react-device-detect"
+import img0 from '../../assets/images/myCollection/0.png'
+import img1 from '../../assets/images/myCollection/1.png'
+import img2 from '../../assets/images/myCollection/2.png'
+import img3 from '../../assets/images/myCollection/3.png'
+import img4 from '../../assets/images/myCollection/4.png'
+import img5 from '../../assets/images/myCollection/5.png'
+import img6 from '../../assets/images/myCollection/6.png'
+import img7 from '../../assets/images/myCollection/7.png'
+import img8 from '../../assets/images/myCollection/8.png'
+import img9 from '../../assets/images/myCollection/9.png'
+import img10 from '../../assets/images/myCollection/10.png'
+import img11 from '../../assets/images/myCollection/11.png'
+import img12 from '../../assets/images/myCollection/12.png'
+import img13 from '../../assets/images/myCollection/13.png'
+import img14 from '../../assets/images/myCollection/14.png'
+import img15 from '../../assets/images/myCollection/15.png'
+import styles from "./index.module.scss"
 
 const chunks = [
   [
     {
       id: 0,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-1.png",
+      image: img0,
     },
     {
       id: 1,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-2.png",
+      image: img1,
     },
     {
       id: 2,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-3.png",
+      image: img2,
     },
     {
       id: 3,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-4.png",
+      image: img3,
     },
   ],
   [
     {
       id: 4,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-5.png",
+      image: img4,
     },
     {
       id: 5,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-6.png",
+      image: img5,
     },
     {
       id: 6,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-7.png",
+      image: img6,
     },
     {
       id: 7,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-8.png",
+      image: img7,
     },
   ],
   [
     {
       id: 8,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-9.png",
+      image: img8,
     },
     {
       id: 9,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-10.png",
+      image: img9,
     },
     {
       id: 10,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-11.png",
+      image: img10,
     },
     {
       id: 11,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-12.png",
+      image: img11,
     },
   ],
   [
     {
       id: 12,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-13.png",
+      image: img12,
     },
     {
       id: 13,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-14.png",
+      image: img13,
     },
     {
       id: 14,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-15.png",
+      image: img14,
     },
     {
       id: 15,
-      image: "https://storage.googleapis.com/beardedbuddies/collection-16.png",
+      image: img15,
     },
   ],
-];
+]
 
 const arr = [
   {
     id: 0,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-1.png",
+    image: img0,
   },
   {
     id: 1,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-2.png",
+    image: img1,
   },
   {
     id: 2,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-3.png",
+    image: img2,
   },
   {
     id: 3,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-4.png",
+    image: img3,
   },
   {
     id: 4,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-5.png",
+    image: img4,
   },
   {
     id: 5,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-6.png",
+    image: img5,
   },
   {
     id: 6,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-7.png",
+    image: img6,
   },
   {
     id: 7,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-8.png",
+    image: img7,
   },
   {
     id: 8,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-9.png",
+    image: img8,
   },
   {
     id: 9,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-10.png",
+    image: img9,
   },
   {
     id: 10,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-11.png",
+    image: img10,
   },
   {
     id: 11,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-12.png",
+    image: img11,
   },
   {
     id: 12,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-13.png",
+    image: img12,
   },
   {
     id: 13,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-14.png",
+    image: img13,
   },
   {
     id: 14,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-15.png",
+    image: img14,
   },
   {
     id: 15,
-    image: "https://storage.googleapis.com/beardedbuddies/collection-16.png",
+    image: img15,
   },
-];
+]
 
 export function NftSection() {
-  const [show, setShow] = useState(4);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [show, setShow] = useState(4)
+  const [currentIndex, setCurrentIndex] = useState(0)
   useEffect(() => {
     if (isMobile) {
-      setShow(1);
-      return;
+      setShow(1)
+      return
     }
-    setShow(4);
-  }, []);
+    setShow(4)
+  }, [])
   return (
     <div id="collection" className={styles.wrapper}>
       <div
@@ -180,7 +196,7 @@ export function NftSection() {
                 ? chunks.map((items, index) => (
                     <div key={`collection-w-${index}`}>
                       <div className={styles.itemsWrapper}>
-                        {items.map((item) => (
+                        {items.map(item => (
                           <div
                             className={styles.itemContainer}
                             key={`collection-${item.id}`}
@@ -197,7 +213,7 @@ export function NftSection() {
                       </div>
                     </div>
                   ))
-                : arr.map((item) => (
+                : arr.map(item => (
                     <div key={`collection-${item.id}`}>
                       <div className={styles.itemWrapper}>
                         <img
@@ -213,7 +229,7 @@ export function NftSection() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default React.memo(NftSection);
+export default React.memo(NftSection)
