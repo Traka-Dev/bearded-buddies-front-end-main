477fdc2e96e80dc15a1ebec59895450116ad6a41
TrakaDev <trakadev@gmail.com>
2022-11-04 16:50:21 -0600

    [REMOVE] Roadmap

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -53,7 +53,6 @@ function Landing() {
         <RedRoseClanSection />
         <BenefitsSection />
         <NftSection />
-        <RoadmapSection />
         <TeamSection />
         <FaqSection />
         <SubscribeSection />
