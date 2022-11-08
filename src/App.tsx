fa2094117bd51c0324681a92bbe6aea3784cc6bf
TrakaDev <trakadev@gmail.com>
2022-11-07 22:14:44 -0600

    [Check Env]

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import styles from "./App.module.scss";
 import Web3Provider from './context/web3Context';
 
 function App() {
+  console.log("ENV")
+  console.dir(process.env.NODE_ENV)
   return (
     <Web3Provider>
     <div className={styles.wrapper}>
