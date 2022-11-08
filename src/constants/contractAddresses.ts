885f5ae673aa11006dab6fa5ac640ec865e70935
TrakaDev <trakadev@gmail.com>
2022-11-07 22:00:41 -0600

    [UPDATE] Smart Contract Addr

--- src/constants/contractAddresses.ts
+++ src/constants/contractAddresses.ts
@@ -3,5 +3,5 @@ if (process.env.NODE_ENV == "production") {
   bearedAddress = "0x9bff6Bc431B3A7d41bdD8142c6BDEd37C5b7b562";
 }
 if (process.env.NODE_ENV == "development") {
-  bearedAddress = "0x780e1E9e9e44aa86724529B6faE782C8Cb3894DE";
+  bearedAddress = "0x9bff6Bc431B3A7d41bdD8142c6BDEd37C5b7b562";
 }
