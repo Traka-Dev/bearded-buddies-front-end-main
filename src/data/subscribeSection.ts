19a2415d28a48cb9209c930ac13071585ff29f84
TrakaDev <trakadev@gmail.com>
2022-12-21 22:01:31 -0600

    [UPDATE] Subscribe Section

--- src/data/subscribeSection.ts
+++ src/data/subscribeSection.ts
@@ -1,8 +1,8 @@
 export const strings = {
     titleOne: "NOT just another",
     titleTwo: "NFT",
-    bodyOne: "Get notified of",
-    bodyTwo: "Bearded Buddies",
-    bodyThree: "news, giveaways and ",
-    bodyFour: "minting dates."
+    bodyOne: "Get Notified about our News, ",
+    bodyTwo: "Personal Growth Posts and videos, ",
+    bodyThree: "Business advices, Partnerships,",
+    bodyFour: "Webinars, and more."
 }
